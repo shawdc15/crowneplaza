ef558a73831cf3ed674b2d0b67fe118fc7052119
shawdc15 <103312452+shawdc15@users.noreply.github.com>
2022-11-14 12:23:57 +0800

    Update ModalLayout.jsx

--- components/Layout/ModalLayout.jsx
+++ components/Layout/ModalLayout.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ModalLayout = ({ children }) => {
   return (
     <div className="fixed top-0 left-0 z-40 flex h-screen w-screen items-center justify-center bg-black/50">
-      <div className="max-w-modal rounded-lg bg-white p-4 py-6 px-7 transition duration-500">
+      <div className="max-h-imageLg max-w-modal overflow-y-auto rounded-lg bg-white p-4 py-6 px-7 transition duration-500">
         {children}
       </div>
     </div>
